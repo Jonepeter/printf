c1e2c90670ce570aabb3737262306c188fc7c5f4
Petros <petros@mtu.edu.et>
2022-05-16 18:25:24 +0300

    please please

--- _printf.c
+++ _printf.c
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-#include "holberton.h"
-=======
 #include "main.h"
->>>>>>> 96fbfcc468d4c6588161cb59876419a3ee5bb00d
+
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
  * Return: the length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
