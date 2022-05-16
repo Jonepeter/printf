6c6f615f23b600fcd8c28cebdb4cd07270d4051b
alexa221 <alextakele16@gmail.com>
2022-05-16 18:09:46 +0300

    header files

--- main.h
+++ main.h
@@ -1,10 +1,6 @@
-#ifndef MAIN
-#define MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
-<<<<<<< HEAD
-
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -21,20 +17,11 @@
  */
 
 typedef struct format
-=======
-/**
-  * struct specifiers - Struct specifiers
-  * @specifier: The conversion specifier
-  * @f: The function pointer
-  */
-typedef struct specifiers
->>>>>>> 0966cd4e722e614964ec675f345b74953bf671f8
 {
-	char *specifier;
-	int (*f)(va_list args);
-} spc_dt;
+	char *id;
+	int (*f)();
+} convert_match;
 
-<<<<<<< HEAD
 int printf_pointer(va_list val);
 int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
@@ -60,34 +47,3 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
-=======
-int _write(char c);
-=======
-int _putchar(char c);
->>>>>>> a251f8ec9a5558c32e9bfc2c463a83a94733ac2c
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_rev(va_list r);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rot13(va_list R);
-
-<<<<<<< HEAD
-#endif /* Group Work Main */
->>>>>>> 0966cd4e722e614964ec675f345b74953bf671f8
-=======
-
-typedef struct code_format
-{
-	char *sc;
-	int (*f)(va_list);
-} code_f;
-
-#endif /* please */
->>>>>>> a251f8ec9a5558c32e9bfc2c463a83a94733ac2c
